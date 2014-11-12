b18deb645f61e9338f723feabc9ca1fa30641317
Glenn Willen <gwillen@nerdnet.org>
2014-11-12 01:31:45 -0800

    rage-specific config

--- config.js
+++ config.js
@@ -2,97 +2,25 @@
 var localhost_config = {
   server_url: "http://localhost:8888/",
   hunt_id: "localtest",
-  gapi_client_id: "368612528023.apps.googleusercontent.com",
-  doc_folder_id: "0B5i1K9hv1-e6VEc0QW9LTHhWVUU",
+  gapi_client_id: "512221203013-mvqk2tlntb8kpi7kbepimevo9j3bve5s.apps.googleusercontent.com",
+  doc_folder_id: "0Bz6L5l-UBC8bZFNKbkVrVTJrXzA",
   jobs_to_display: ["Puzzle Czar", "Comm Czar"],
   hunt_url: "http://en.wikipedia.org/wiki/Puzzlehunt",
   hunt_info: "no login necessary",
   team_url: "http://en.wikipedia.org/wiki/Team",
 };
 
-// Configuration for Team Left Out, MIT Mystery Hunt 2013.
-var leftout13_config = {
-  server_url: "http://czar.emagino.net:433/",
-  hunt_id: "mh2013",
-  gapi_client_id: "368612528023-n65vkgeithp9k7ch3nr2e9rubnjqj5ib.apps.googleusercontent.com",
-  doc_folder_id: "0B5i1K9hv1-e6TTE2LXpwYk16QzA",
+// Configuration for Team Rage, MIT Mystery Hunt 2015.
+var rage15_config = {
+  server_url: "http://rotq.net:8888/",
+  hunt_id: "mh2015_rage",
+  gapi_client_id: "512221203013-mvqk2tlntb8kpi7kbepimevo9j3bve5s.apps.googleusercontent.com",
+  doc_folder_id: "0Bz6L5l-UBC8bNFE0LWJhYldnNWs",
   jobs_to_display: ["Puzzle Czar", "Comm Czar"],
-  hunt_url: "http://enigmavalley.com/",
-  hunt_info: "user:leftout pass:leftierthanthou",
-  team_url: "https://sites.google.com/a/teamleftout.org/mh2013/hunt",
-};
-
-// Configuration for Burninators, Stanford Hunt 2013.
-var stanford13_config = {
-  server_url: "http://czar.emagino.net:8080/",
-  hunt_id: "stanford2013",
-  gapi_client_id: "368612528023-n65vkgeithp9k7ch3nr2e9rubnjqj5ib.apps.googleusercontent.com",
-  doc_folder_id: "0B7WUjr1PseBmdk9ybzFEU3hLSlE",
-  jobs_to_display: [],
-  hunt_url: "https://chaosinitiative.com/puzzles",
-  hunt_info: "user:burninators password: profitsnoddedcondemned",
-  team_url: "",
-};
-
-// Configuration for Demonic Robot Tyrannosaurs, Berkeley Hunt 2013.
-var berkeley13_config = {
-  server_url: "http://czar.emagino.net:433/",
-  hunt_id: "berkeley2013",
-  gapi_client_id: "368612528023-n65vkgeithp9k7ch3nr2e9rubnjqj5ib.apps.googleusercontent.com",
-  doc_folder_id: "0BwXGC8wDbipUd2tyWUlvT05WMjg",
-  jobs_to_display: [],
-  hunt_url: "https://ankursresearch.com/",
-  hunt_info: "user:Demonic Robot Tyrannosaurs password: fgd",
-  team_url: "",
-};
-
-// Configuration for Team Left Out, MIT Mystery Hunt 2014.
-var leftout14_config = {
-  server_url: "http://czar.emagino.net:433/",
-  hunt_id: "mh2014",
-  gapi_client_id: "368612528023-n65vkgeithp9k7ch3nr2e9rubnjqj5ib.apps.googleusercontent.com",
-  doc_folder_id: "0B5i1K9hv1-e6TWtoVFJUSXZ4bzQ",
-  jobs_to_display: ["Puzzle Czar", "Comm Czar"],
-  hunt_url: "http://www.aliceshrugged.com/",
-  hunt_info: "user:teamleftout pass:leftierthanthou",
-  team_url: "https://sites.google.com/a/teamleftout.org/mh2014/hunt",
-};
-
-// Configuration for Objects in Mirror, Microsoft Puzzle Hunt 2014.
-var msph2014_config = {
-  server_url: "http://czar.emagino.net:433/",
-  hunt_id: "msph2014",
-  gapi_client_id: "368612528023-n65vkgeithp9k7ch3nr2e9rubnjqj5ib.apps.googleusercontent.com",
-  doc_folder_id: "0BwXGC8wDbipUbHliWTZNSFZvRkU",
-  jobs_to_display: [],
-  hunt_url: "http://puzzlehunt/15/",
-  hunt_info: "user:<unknown> pass:<unknown>",
-  team_url: "http://www.pavelspuzzles.com/",
-};
-
-// Configuration for Demonic Robot Tyrannosaurs, Berkeley Hunt 2013.
-var berkeley14_config = {
-  server_url: "http://czar.emagino.net:433/",
-  hunt_id: "berkeley2014",
-  gapi_client_id: "368612528023-n65vkgeithp9k7ch3nr2e9rubnjqj5ib.apps.googleusercontent.com",
-  doc_folder_id: "0BwXGC8wDbipUNDZnQkR5eDluQU0",
-  jobs_to_display: [],
-  hunt_url: "https://puzzle.calumnio.us/",
-  hunt_info: "user:Demonic Robot Tyrannosaurs password: roar",
-  team_url: "",
-};
-
-// Configuration for Burninators, SUMS 2013.
-var sums14_config = {
-  server_url: "http://czar.emagino.net:433/",
-  hunt_id: "sums2014",
-  gapi_client_id: "368612528023-n65vkgeithp9k7ch3nr2e9rubnjqj5ib.apps.googleusercontent.com",
-  doc_folder_id: "0BwXGC8wDbipUdHNXZHhOTGFGaEk",
-  jobs_to_display: [],
-  hunt_url: "http://www.maths.usyd.edu.au/ub/sums/puzzlehunt/2014/puzzles",
-  hunt_info: "user:Burninators password: fgd",
-  team_url: "",
+  hunt_url: "http://example.com/",
+  hunt_info: "user:rage pass:ireproof",
+  team_url: "http://rotq.net/",
 };
 
 // Modify this to use a different config.
-var config = sums14_config
+var config = localhost_config
